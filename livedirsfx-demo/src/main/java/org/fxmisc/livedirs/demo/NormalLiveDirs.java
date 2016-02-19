0d80fd28d0ef1f0479d93685bd22286e8030cb71
JordanMartinez <jordanalex.martinez@gmail.com>
2016-02-19 14:23:07 -0800

    Added demo for LiveDirs instances that don't display items as though they were CheckBoxTreeItems

---
+++ livedirsfx-demo/src/main/java/org/fxmisc/livedirs/demo/NormalLiveDirs.java
@@ -0,0 +1,38 @@
+package org.fxmisc.livedirs.demo;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TreeView;
+import javafx.stage.Stage;
+import org.fxmisc.livedirs.LiveDirs;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NormalLiveDirs extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        TreeView<Path> view = new TreeView<>();
+        view.setShowRoot(false);
+
+        try {
+            // create a LiveDirs instance for use on the JavaFX Application Thread
+            LiveDirs<ChangeSource, Path> dirs = LiveDirs.getNormalInstance(ChangeSource.EXTERNAL);
+
+            // set directory to watch
+            dirs.addTopLevelDirectory(Paths.get(System.getProperty("user.home"), "Documents").toAbsolutePath());
+            view.setRoot(dirs.model().getRoot());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        primaryStage.setScene(new Scene(view, 500, 500));
+        primaryStage.show();
+    }
+}
